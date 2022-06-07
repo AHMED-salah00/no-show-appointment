231f70503d2f8180b8d5efb752aaeb9e8479f2f7
AHMED-salah00 <72524423+AHMED-salah00@users.noreply.github.com>
2022-06-07 15:17:26 +0200

    Add files via upload

---
+++ bikeshare_2.py
@@ -0,0 +1,27 @@
+import time
+import pandas as pd
+import numpy as np
+
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+
+def get_filters():
+    """
+    Asks user to specify a city, month, and day to analyze.
+
+    Returns:
+        (str) city - name of the city to analyze
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
+    """
+    print('Hello! Let\'s explore some US bikeshare data!')
+    # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
+    city = input('Hii,,, please choose a city from the following,,,(chicago, new york city, washington):')
+    while city in ("chicago", "new york city", "washington"):
+        print('you chosed:',city)
+    else:
+        print('please choose a correct city from (chicago, new york city, washington)!!!')
+
+
+    get_filters()
